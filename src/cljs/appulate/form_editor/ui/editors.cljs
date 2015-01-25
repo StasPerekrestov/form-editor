504d65e17125978ae43cb658aab24db74c056db7
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2015-01-25 19:25:00 +0200

    yes-no control

--- src/cljs/appulate/form_editor/ui/editors.cljs
+++ src/cljs/appulate/form_editor/ui/editors.cljs
@@ -11,11 +11,19 @@
     (render [_]
       (dom/input #js {:type "text" :value value :placeholder "Type value here"}))))
 
-(defmethod ua-control :yesno [{:keys [value]}]
+(defn label-for-radio [title]
+  (dom/span #js {:style #js {:margin "10px"}} title))
+
+(defmethod ua-control :yesno [{:keys [id value]}]
   (reify
     om/IRender
     (render [_]
-      (dom/input #js {:type "checkbox" :checked value}))))
+      (let [radio-name (str "radio" id)]
+        (dom/div nil
+          (dom/label #js {:className "left"}
+            (dom/input #js {:type "radio" :name radio-name}) (label-for-radio "Yes"))
+          (dom/label nil
+            (dom/input #js {:type "radio" :name radio-name}) (label-for-radio "No")))))))
 
 (defmethod ua-control :default [{type :type}]
   (throw
