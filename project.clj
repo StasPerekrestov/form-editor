33705983bf99475c79390f2cd57355a315d97a2b
Stanislav Perekrestov <stanislav.perekrestov@muranosoft.com>
2015-02-11 19:27:12 +0200

    cljs is updated

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [ring/ring-defaults "0.1.3"]
                  [javax.servlet/servlet-api "2.5"]
                  ;; CLJS
-                 [org.clojure/clojurescript "0.0-2760"]
+                 [org.clojure/clojurescript "0.0-2816"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  ;[cljs-http "0.1.25"]
                  [secretary "1.2.1"]
